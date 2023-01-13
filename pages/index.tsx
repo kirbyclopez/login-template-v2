e2aa856a388a1c22e61727c9ad671ce9fbc44673
kirbyclopez <kirbyclopez@gmail.com>
2023-01-13 14:11:41 +0800

    feat: added BaseTemplate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="bg-slate-500 text-3xl font-bold underline">Home Page</div>
-  );
+  return <div className="bg-slate-500 text-3xl font-bold underline">Home</div>;
 }
