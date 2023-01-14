d47289e24e02f08c5a17de50e969bbdd65a80c79
kirbyclopez <kirbyclopez@gmail.com>
2023-01-14 23:35:19 +0800

    feat: added logout function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
+import axios from 'axios';
+import Router from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
+  const handleLogout = async () => {
+    await axios.delete('http://localhost:5000/api/auth/sessions', {
+      withCredentials: true,
+    });
+
+    Router.push('/login');
+  };
+
   return (
     <div>
       <h1>This is the landing page.</h1>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
